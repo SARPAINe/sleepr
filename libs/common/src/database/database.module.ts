0fd75e0c5f49112dbf74f866bcf0b4d92ebd8e94
shaharin <ahmedshaharin7589@gmail.com>
2025-04-11 17:04:51 +0600

    feat(config): integrate ConfigModule with environment validation
    Enhancements include:
    - Added `JwtModule` and `ConfigModule` to `AuthModule` with async configuration
    - Implemented environment variable validation using Joi in both `AuthModule` and `ReservationsModule`
    - Updated application bootstrap files to utilize `ConfigService` for port configuration
    - Modified Docker configurations to use environment files
    - Removed redundant custom `ConfigModule`

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: (configServe: ConfigService) => ({
         uri: configServe.get('MONGODB_URI'),
       }),
