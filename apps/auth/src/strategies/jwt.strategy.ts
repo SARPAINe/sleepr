63dc6b69294217504979c8fd7ed52ea704c97850
Shaharin Ahmed <ahmedshaharin7589@gmail.com>
2025-04-13 13:47:03 +0600

    feat: add jwt strategy to get authenticated user

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from '../interfaces/token-payload.interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.Authentication,
+      ]),
+      secretOrKey:
+        configService.get<string>('JWT_SECRET') ||
+        (() => {
+          throw new Error('JWT_SECRET is not defined');
+        })(),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.usersService.getUser({ _id: userId });
+  }
+}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,15 +2,28 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
   async createUser(createUserDto: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDto);
     return this.userRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
     });
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+    if (!email || !password) {
+      throw new UnauthorizedException('Email and password are required');
+    }
+    const existingUser = await this.userRepository.findOne({ email });
+    if (existingUser) {
+      throw new UnauthorizedException('Email already exists');
+    }
   }
 
   async verifyUser(email: string, password: string) {
@@ -27,5 +40,9 @@ export class UsersService {
 
   async getUsers() {
     return this.userRepository.find({});
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.userRepository.findOne(getUserDto);
   }
 }
